eb8659d13ac4def11c4607c5fb03028ef472c4b1
ericsmith92 <ericsmth884@gmail.com>
2025-04-24 09:33:04 -0400

    feat(findPlaces): add pagination to places querying

--- scripts/findPlaces.js
+++ scripts/findPlaces.js
@@ -3,6 +3,7 @@ const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
 
 const client = new Client({});
+const ST_LAWRENCE_LOCATION = "43.651067,-79.370661"; // Center of St. Lawrence
 
 async function seedDatabase() {
     try {
@@ -20,9 +21,6 @@ async function seedDatabase() {
 }
 
 async function findPlaces() {
-    // Define coordinates for St. Lawrence neighborhood
-    const stLawrenceLocation = "43.651067,-79.370661"; // Center of St. Lawrence
-    
     // Define multiple cuisine queries to get around the 20 result limit
     const cuisineQueries = [
         'Restaurants in St. Lawrence Toronto',
@@ -34,43 +32,52 @@ async function findPlaces() {
     
     let allPlaces = [];
     const seenPlaceIds = new Set();
+
+    const params = {
+        location: ST_LAWRENCE_LOCATION,
+        radius: 1000, // meters
+        type: 'restaurant',
+        language: 'en-CA',
+        key: process.env.GOOGLE_MAPS_API_KEY
+    }
     
     // Run multiple queries to build our database
     for (const query of cuisineQueries) {
+        params.query = query;
+
         try {
             console.log(`Fetching results for query: "${query}"`);
-            
-            const response = await client.textSearch({
-                params: {
-                    query: query,
-                    location: stLawrenceLocation,
-                    radius: 1000, // meters
-                    type: 'restaurant',
-                    language: 'en-CA',
-                    key: process.env.GOOGLE_MAPS_API_KEY
-                }
-            });
+            let allPages = [];
+            const firstPage = await getPlaces(params);
 
+            allPages.push(firstPage);
 
-            console.log(response);
-            
-            if (response.data.results && response.data.results.length > 0) {
-                // Filter out places we've already seen (by place_id)
-                const newPlaces = response.data.results.filter(place => {
-                    console.log('*****************************************************');
-                    console.log(place);
-                    console.log('*****************************************************');
-                    if (!seenPlaceIds.has(place.place_id)) {
-                        seenPlaceIds.add(place.place_id);
-                        return true;
-                    }
-                    return false;
-                });
-                
-                allPlaces = [...allPlaces, ...newPlaces];
-                console.log(`Added ${newPlaces.length} new places from this query`);
-            } else {
-                console.log('No results for this query');
+            let nextPageToken = firstPage.data.next_page_token;
+
+            while(nextPageToken){
+                await new Promise(r => setTimeout(r, 2000)); 
+                const nextPage = await getPlaces({ pagetoken: nextPageToken, key: process.env.GOOGLE_MAPS_API_KEY });
+                allPages = [...allPages, nextPage];
+                nextPageToken = nextPage.data.next_page_token;
+            }
+
+            for(const page of allPages){
+                if (page.data.results && page.data.results.length > 0) {
+                    // Filter out places we've already seen (by place_id)
+                    const newPlaces = page.data.results.filter(place => {
+                        if (!seenPlaceIds.has(place.place_id)) {
+                            seenPlaceIds.add(place.place_id);
+                            return true;
+                        }
+    
+                        return false;
+                    });
+                    
+                    allPlaces = [...allPlaces, ...newPlaces];
+                    //console.log(`Added ${newPlaces.length} new places from this query`);
+                } else {
+                    console.log('No results for this query');
+                }    
             }
             
             // Add a delay to avoid hitting rate limits
@@ -82,6 +89,18 @@ async function findPlaces() {
     }
     
     return allPlaces;
+}
+
+async function getPlaces(params){  
+    const response = await client.textSearch({
+        params
+    });
+
+    return response;
+}
+
+function filterPlaces(page, seenPlaceIds){
+
 }
 
 seedDatabase()
