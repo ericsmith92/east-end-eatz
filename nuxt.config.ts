1a075fc95646a2b58ecd3329215a79cedbd3687b
ericsmith92 <ericsmth884@gmail.com>
2025-05-21 06:44:36 -0400

    chore: TS config stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint'],
+  typescript: {
+    typeCheck: true
+  }
 })
