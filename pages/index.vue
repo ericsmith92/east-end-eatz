0593fad484fa1dc2721ddac435ce8df2a389e22d
ericsmith92 <ericsmth884@gmail.com>
2025-06-09 06:09:39 -0400

    feat(pagination): add padding and center pagination buttons

--- pages/index.vue
+++ pages/index.vue
@@ -11,17 +11,19 @@ await callOnce(() => fetchPage())
   <div class="p-4">
     <h1 class="text-2xl font-bold mb-4">East End Restaurants</h1>
     <RestaurantList :restaurants="placesStore.list" />
-    <PaginationButton
-      :onClick="getPreviousPage"
-      ariaLabel="Previous"
-      label="Previous"
-      :disabled="!placesStore.offset"
-    />
-    <PaginationButton
-      :onClick="getNextPage"
-      ariaLabel="Next"
-      label="Next"
-      :disabled="!placesStore.hasNextPage"
-    />
+    <div class="flex justify-center gap-4 mt-8 max-w-6xl mx-auto">
+      <PaginationButton
+        :onClick="getPreviousPage"
+        ariaLabel="Previous"
+        label="Previous"
+        :disabled="!placesStore.offset"
+      />
+      <PaginationButton
+        :onClick="getNextPage"
+        ariaLabel="Next"
+        label="Next"
+        :disabled="!placesStore.hasNextPage"
+      />
+    </div>
   </div>
 </template>
